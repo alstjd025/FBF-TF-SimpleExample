ffd7bcf06be03246f10ce0af7ab76fee95378321
Minsung Kim <alstjd025@naver.com>
2021-09-07 14:19:24 +0900

    bash modify

--- start_a.sh
+++ start_a.sh
@@ -10,5 +10,4 @@ echo "TfLite Unit_simple Test"
 . ${TflitePath}/build_aarch64_lib.sh
 touch unit_simple.cc
 make
-.unit_simple mnist_original_102030.tflite
 
--- unit_simple.cc
+++ unit_simple.cc
@@ -1,4 +1,6 @@
 #include "tensorflow/lite/unit_handler.h"
+#define SEQ 10000
+#define OUT_SEQ 1
 
 using namespace cv;
 using namespace std;
@@ -16,19 +18,18 @@ void read_Mnist(string filename, vector<cv::Mat>& vec) {
 	ifstream file(filename, ios::binary);
 	if (file.is_open()){
 		int magic_number = 0;
-		int number_of_images = 1;
+		int number_of_images = 0;
 		int n_rows = 0;
 		int n_cols = 0;
 		file.read((char*)& magic_number, sizeof(magic_number));
 		magic_number = ReverseInt(magic_number);
 		file.read((char*)& number_of_images, sizeof(number_of_images));
-		//number_of_images = ReverseInt(number_of_images);
-		number_of_images = 1;
+		number_of_images = ReverseInt(number_of_images);
         file.read((char*)& n_rows, sizeof(n_rows));
 		n_rows = ReverseInt(n_rows);
 		file.read((char*)& n_cols, sizeof(n_cols));
 		n_cols = ReverseInt(n_cols);
-		for (int i = 0; i < number_of_images; ++i){
+		for (int i = 0; i < SEQ; ++i){
 			cv::Mat tp = Mat::zeros(n_rows, n_cols, CV_8UC1);
 			for (int r = 0; r < n_rows; ++r){
 				for (int c = 0; c < n_cols; ++c){
@@ -54,7 +55,7 @@ void read_Mnist_Label(string filename, vector<unsigned char> &arr) {
 			unsigned char temp = 0;
 			file.read((char*)&temp, sizeof(temp));
 			if (i > 7) {
-				//cout << (int)temp << " ";
+				cout << (int)temp << " ";
 				arr.push_back((unsigned char)temp);
 			}
 		}
